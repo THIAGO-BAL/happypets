ba21276144ec2485dfff9f7cc43d7ce02d14d64f
Victor Stillo <victor.stillo@gmail.com>
2017-01-17 11:47:18 -0200

    Criação de classes que controlam o table view de adoção

---
+++ HappyPets/AdocaoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AdocaoTableViewCell.swift
+//  HappyPets
+//
+//  Created by Student on 1/17/17.
+//  Copyright © 2017 thiago-Student. All rights reserved.
+//
+
+import UIKit
+
+class AdocaoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
